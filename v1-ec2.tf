415435a4db1455f928bee94b93ad5371f7040f20
Sachin Arora <sacharora999@gmail.com>
2024-07-13 07:01:32 +0530

    terraform proj

---
+++ v1-ec2.tf
@@ -0,0 +1,44 @@
+provider "aws" {
+region  = "ap-south-1"
+}
+
+
+resource "aws_instance" "web" {
+  ami            = "ami-0ec0e125bb6c6e8ec"
+  instance_type  = "t2.micro"
+  key_name       = "samplekey"
+  security_groups = [ "allow_tls" ]
+}
+
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = "vpc-04ac708b00ec00340"
+
+  tags = {
+    Name = "allow_tls_ssh"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
